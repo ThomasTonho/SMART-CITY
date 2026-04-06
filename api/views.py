fc1097a1760ae0c32411736c380888b5ad126c29
ThomasTonho <antonellithomasv@gmail.com>
2026-04-06 10:13:36 -0300

    VIEWS

--- api/views.py
+++ api/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from .models import *
+from .serializers import *
+from rest_framework import viewsets
 
-# Create your views here.
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+
+class LocaisViewSet(viewsets.ModelViewSet):
+    queryset = Locais.objects.all()
+    serializer_class = LocaisSerializer
+
+class ResponsaveisViewSet(viewsets.ModelViewSet):
+    queryset = Responsaveis.objects.all()
+    serializer_class = ResponsaveisSerializer
+
+class AmbientesViewSet(viewsets.ModelViewSet):
+    queryset = Ambientes.objects.all()
+    serializer_class = AmbientesSerializer
+
+class MicrocontroladoresViewSet(viewsets.ModelViewSet):
+    queryset = Microcontroladores.objects.all()
+    serializer_class = MicrocontroladoresSerializer
+
+class SensoresViewSet(viewsets.ModelViewSet):
+    queryset = Sensores.objects.all()
+    serializer_class = SensoresSerializer
+
+class HistoricosViewSet(viewsets.ModelViewSet):
+    queryset = Historicos.objects.all()
+    serializer_class = HistoricosSerializer
+
+
