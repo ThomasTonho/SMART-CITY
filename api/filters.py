a319f42261549d3921be6c5da2d73558cd488ff1
ThomasTonho <antonellithomasv@gmail.com>
2026-04-13 08:07:51 -0300

    FILTRO LOCAIS

--- api/filters.py
+++ api/filters.py
@@ -11,3 +11,9 @@ class UsuarioFilter(django_filters.FilterSet):
         model = Usuario
         fields = ['nome', 'telefone', 'tipo']
 
+class LocaisFilter(django_filters.FilterSet):
+    nome = django_filters.CharFilter(field_name='nome', lookup_expr='icontains')
+
+    class Meta:
+        model = Locais
+        fields = ['nome']
