8795ec85ff030b044b24e466823ae2da1274d3c8
ThomasTonho <antonellithomasv@gmail.com>
2026-04-13 08:09:00 -0300

    FILTRO AMBIENTES

--- api/filters.py
+++ api/filters.py
@@ -24,3 +24,13 @@ class ResponsaveisFilter(django_filters.FilterSet):
     class Meta:
         model = Responsaveis
         fields = ['nome']
+
+class AmbientesFilter(django_filters.FilterSet):
+    local = django_filters.CharFilter(field_name='local__nome', lookup_expr='icontains')
+    responsavel = django_filters.CharFilter(field_name='responsavel__nome', lookup_expr='icontains')
+    descricao = django_filters.CharFilter(field_name='descricao', lookup_expr='icontains')
+
+    class Meta:
+        model = Ambientes
+        fields = ['local', 'responsavel', 'descricao']
+
