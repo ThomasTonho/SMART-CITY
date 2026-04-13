46ac7c6cc57225a37f2ec78334b563e8640c05f5
ThomasTonho <antonellithomasv@gmail.com>
2026-04-13 09:48:55 -0300

    feat: implementa estrutura inicial do banco de dados

--- api/views.py
+++ api/views.py
@@ -5,40 +5,52 @@ from rest_framework import viewsets
 from .filters import *
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import  action, permission_classes
+from django_filters.rest_framework import DjangoFilterBackend
 
 class UsuarioViewSet(viewsets.ModelViewSet):
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
+
+    filter_backends = [DjangoFilterBackend]
     filterset_class = UsuarioFilter
 
 class LocaisViewSet(viewsets.ModelViewSet):
     queryset = Locais.objects.all()
     serializer_class = LocaisSerializer
+
+    filter_backends = [DjangoFilterBackend]
     filterset_class = LocaisFilter
 
 class ResponsaveisViewSet(viewsets.ModelViewSet):
     queryset = Responsaveis.objects.all()
     serializer_class = ResponsaveisSerializer
+
+    filter_backends = [DjangoFilterBackend]
     filterset_class = ResponsaveisFilter
 
 class AmbientesViewSet(viewsets.ModelViewSet):
     queryset = Ambientes.objects.all()
     serializer_class = AmbientesSerializer
+
+    filter_backends = [DjangoFilterBackend]
     filterset_class = AmbientesFilter
 
 class MicrocontroladoresViewSet(viewsets.ModelViewSet):
     queryset = Microcontroladores.objects.all()
     serializer_class = MicrocontroladoresSerializer
+
     filterset_class = MicrocontroladoresFilter
 
 class SensoresViewSet(viewsets.ModelViewSet):
     queryset = Sensores.objects.all()
     serializer_class = SensoresSerializer
+
     filterset_class = SensoresFilter
 
 class HistoricosViewSet(viewsets.ModelViewSet):
     queryset = Historicos.objects.all()
     serializer_class = HistoricosSerializer
+
     filterset_class = HistoricosFilter
 
 
